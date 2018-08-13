16aa84c4b46c05a2f8bb9cc3a417049e0c5a566c
jgchenu <jgchenu@foxmail.com>
2018-08-13 12:05:46 +0800

    更新了token登录

--- js/course.js
+++ js/course.js
@@ -1,122 +1,87 @@
-// layui模块
-layui.use(['jquery','layer','element'], function(){ 
-  var layer = layui.layer;
-  var element = layui.element();
-  var status= "course";
-  var today=new Date();
-  $.support.cors =true; 
-  // 画面载入时加载动画
-  layer.load();
-        setTimeout(function(){
-          layer.closeAll('loading');
-        }, 300);
-  // 课程表添加按钮的函数
-  $('#add_btn').click(function(){
-  	layer.open({
-  	type: 1, 
-  	content: $('#add_course'),
-  	scrollbar: false,
-  	anim:2,
-    shadeClose:true,
-    closeBtn:2,
-  	btn:['保存'],
-  	yes:function(index,layero){
-  		var $add_course=$("#add_course"),$rows=$("tr.row"),color_num=parseInt(Math.random()*9+1);
-  		var weekday=$add_course.find("select[name='weekday'] option:selected").val(),
-  		course_num=$add_course.find("select[name='course_num'] option:selected").val(),
-  		course_place=$add_course.find("input[name='course_place']").val(),
-  		course_name=$add_course.find("input[name='course_name']").val();
-  		console.log(course_place);
-      if(weekday==0||course_num==0||course_name==''){
-        layer.msg('添加失败',{
-        icon:5,
-        time:1000
-      });
-      }else{
-        $rows.eq(course_num).find("td").eq(weekday).removeClass().addClass("color"+color_num).html(course_name+"<br>"+course_place+"<br>");
-      layer.msg('添加成功',{
-        icon:6,
-        time:1000
-      });
-      layer.close(index);
-      }
-  	},
-});
-    // 弹出窗口函数结束
-}); 
-  // 点击弹出按钮函数结束
- 
-// 点击切换类型按钮开始
-  $("#table_btn").click(function(){
-  		$("#course_list").hide();
-		  $(".desk_table").show();
-      $("body").css('background','#fff');
-		
-	});
-	$("#list_btn").click(function(){
-		$(".desk_table").hide();
-		$("#course_list").show();
-    $("body").css('background','#eceff6');
-	});
-// 点击切换按钮结束
+layui.use(['jquery', 'layer', 'element'], function() {
+    var layer = layui.layer;
+    var element = layui.element();
+    var status = "course";
+    var today = new Date();
+    var token = localStorage.getItem('kbtoken');
+    if (!localStorage.getItem('kbtoken')) { window.location.href = "index.html"; }
+    if (!$.cookie("stu_id") || !$.cookie("stu_name") || !localStorage.getItem('kbtoken')) { window.href = "index.html"; }
+    $.support.cors = true;
+    layer.load(2);
+    setTimeout(function() {
+        layer.closeAll('loading');
+    }, 1000);
+    $("#table_btn").click(function() {
+        $("#course_list").hide();
+        $(".desk_table").show();
+        $("body").css('background', '#fff');
 
-// 点击重置课表执行函数
-  $("#load_course").click(function(){
+    });
+    $("#list_btn").click(function() {
+        $(".desk_table").hide();
+        $("#course_list").show();
+        $("body").css('background', '#eceff6');
+    });
+
+    $("#load_course").click(function() {
         $.cookie("remember", "false", { expire: -1 });
         $.cookie("stu_id", "", { expires: -1 });
         $.cookie("stu_name", "", { expires: -1 });
-        window.location.href="index.html";
-  });
+        window.location.href = "login.html";
+    });
 
-  // ajax 加载课程表的信息
-  $.ajax({
-            type:'POST', //请求方式，默认get
-            url :"http://keepfriend.cn/kcb/course_schedule.php",
-            data:{stu_id:$.cookie("stu_id"),stu_name:$.cookie("stu_name"),status:status},
-            dataType: "json", 
-            // 请求成功时的回调函数
-            success:function(data){ 
-                if(data.header.status=="0"){
+
+    $.ajax({
+        type: 'POST',
+        url: "http://www.szer.me/kcb/course_schedule.php",
+        data: { stu_id: $.cookie("stu_id"), stu_name: $.cookie("stu_name"), status: status, 'token': token },
+        dataType: "json",
+        success: function(data) {
+            console.log(data);
+            if (data.header.status == "0") {
                 layer.msg(data.header.extra_info);
-                }else if(data.header.status=="1"){
-                    //遍历每一行 
-               $.each(data.data,function(key,val){
-                  // 遍历每一列
-                  $.each(val,function(index,value){
-                    if(!value.course_name||!value.course_place){
-                       value.course_place="";
-                       value.course_name="";
-                    }
-                        var color_num=parseInt(Math.random()*9+1);
-                        // 加载表格课程
-                        $("tr.row").eq(parseInt(key)).find("td").eq(parseInt(index)).removeClass().addClass("color"+color_num)
-                        .html(value.course_name+"<br>"+value.course_place); 
-                        // 加载列表课程
-                        $("div.col").eq(parseInt(index)-1).find("p").eq(parseInt(key)-1)
-                        .html(value.course_name+"@"+value.course_place);
-                        // 判断表格没有课程的格子不要加载颜色，判断没有课程的列表的节数不要显示
-                        if(value.course_name==""||value.course_place==""){
-                        $("tr.row").eq(parseInt(key)).find("td").eq(parseInt(index)).removeClass();
-                        $("div.col").eq(parseInt(index)-1).find("p").eq(parseInt(key)-1).parent().hide();
+            } else if (data.header.status == "1") {
+
+                var $table = $("table.desk_table");
+                var $list = $("div#course_list");
+                $.each(data.desk, function(key, val) {
+                    $table.append("<tr><td class='tr_color'>" + key + "</td></tr>");
+                    $.each(val, function(index, value) {
+                        if (!value.course_name || !value.course_place) {
+                            value.course_place = "";
+                            value.course_name = "";
                         }
-                        // 判断今天是星期几，将对应的课程展开
-                        if(today.getDay()<5){
-                          $("div.col").eq(today.getDay()).addClass("layui-show");    
-                        }  
-                                                       
-                  });
-                  //遍历每一行结束
-               });
-               // 遍历每一列结束
-              }
+                        if (value.course_continuous) {
+                            return true;
+                        }
+                        var $tr = $table.find("tr").eq(key);
+                        $tr.append("<td" + " rowspan=" + value.course_duration + " class=color" + (value.course_id + 1) + ">" + value.course_name + "<br/>" + value.course_place + "</td>");
+                    });
+                });
 
-              $("h1.title span").html(data.header.week_info);
+                $.each(data.list, function(key, val) {
+                    var col = $list.find(".col").eq(key - 1);
+                    if (val.length == 0) {
+                        col.append("<div>无课</div>");
+                    } else {
 
-              console.log(data.header);
-            },error:function(data){
-               layer.msg("加载出现错误！可能是网络问题");
-               // 请求失败弹出提示
+
+                        $.each(val, function(index, value) {
+                            col.append("<div><h3>" + value.course_section + "</h3>" +
+                                "<p>" + value.course_name + "@" + value.course_place + "</p></div>");
+                        });
+                    }
+                });
             }
-         });
+
+            $("h1.title").html("第" + data.header.week_info + "周");
+            if (1 < today.getDay() && today.getDay() < 6) {
+                $("div.col").eq(today.getDay() - 1).addClass("layui-show");
+            }
+        },
+        error: function(data) {
+            layer.msg("加载出现错误！可能是网络问题");
+        }
+    });
 
 });
--- js/login.js
+++ js/login.js
@@ -1,72 +1,136 @@
+layui.use(['layer'], function() {
+    var layer = layui.layer;
+    var status = "login";
+    var token;
+    var code;
+    $("#remember_me").attr('checked', 'checked');
+    $.support.cors = true;
 
-layui.use(['layer'], function(){ 
-		var layer = layui.layer;
-		var status="login";
-		$.support.cors =true;
-		 //默认点开窗口时是点击记住我 
-		$("#remember_me").attr('checked', 'checked');
-		//如果点了记住我，直接获取cookie的值
-		if ($.cookie("remember") == "true") {
-	    $.ajax({
-		type:'POST', //请求方式，默认POST
-        url :"http://keepfriend.cn/kcb/course_schedule.php",
-        data:{stu_id:$.cookie("stu_id"),stu_name:$.cookie("stu_name"),status:status},
-        // data:stu_id:$("#stu_id").val(),stu_name:$("#stu_name").val(),status:status,
-        dataType: "json", 	
-        success:function(data){
-        	console.log(data.header);
-        	if(data.header.status=="0"){
-        		layer.msg(data.header.extra_info);
-        	}else if(data.header.status=="1"){
-        		window.location.href="course.html";
-        	}
-        	
-        },error:function(){
-        		layer.msg("导入出现错误！可能是网络问题");
-        },
-		});
-	    }
+    function getQueryString(name) {
+        var reg = new RegExp("(^|&)" + name + "=([^&]*)(&|$)", "i");
+        var r = window.location.search.substr(1).match(reg);
+        if (r != null) return unescape(r[2]);
+        return null;
+    }
 
-//导入课程
-		$("button.btn-default").click(function(){
-			var stu_id = $("#stu_id").val();
-	      	var stu_name = $("#stu_name").val();
-			layer.load();
-		    setTimeout(function(){
-		      layer.closeAll('loading');
-		    }, 300);
-		  if ($("#remember_me").is(':checked')) {
-		 	console.log("我打沟了！");
-	      $.cookie("remember", "true", { expires: 180 }); //存储一个带180天期限的cookie
-	      $.cookie("stu_id",stu_id, { expires: 180 });
-	      $.cookie("stu_name",stu_name, { expires: 180 });
+    if (localStorage.getItem('kbtoken')) {
+        token = localStorage.getItem('kbtoken');
+        if ($.cookie("remember") == "true") {
+            $.ajax({
+                type: 'POST',
+                url: "http://www.szer.me/kcb/course_schedule.php",
+                data: { 'stu_id': $.cookie("stu_id"), 'stu_name': $.cookie("stu_name"), status: status, 'token': token },
+                dataType: "json",
+                success: function(data) {
+                    console.log(data);
+                    if (data.header.status == "0") {
+                        layer.msg(data.header.extra_info);
+                    } else if (data.header.status == "1") {
+                        window.location.href = "course.html";
+                    }
+
+                },
+                error: function(data) {
+                    layer.msg("导入出现错误！请检查数据格式");
+                },
+            });
+        }
+    } else {
+        code = getQueryString('code');
+        if (!code) { window.location.href = "index.html" } else {
+            $.ajax({
+                type: 'POST',
+                url: "http://www.szer.me/kcb/api/token.php",
+                dataType: "json",
+                data: { 'code': code },
+                success: function(data) {
+                    console.log(data);
+                    localStorage.setItem('kbtoken', data.retdata.token);
+                    console.log(data.retdata.token);
+                    token = localStorage.getItem('kbtoken');
+                },
+                error: function(data) {
+                    console.log('error:' + data);
+                }
+            });
+        }
+
     }
-    else {
-    	  console.log("我没打沟..");
-	      $.cookie("remember", "false", { expire: 1 });
-	      $.cookie("stu_id", stu_id, { expires: 1 });
-	      $.cookie("stu_name", stu_name, { expires: 1 });
+
+
+
+    var stu_id = $("#stu_id").val();
+    var stu_name = $("#stu_name").val();
+    var reg = /^\d+$/;
+    if (!stu_id || !stu_name || !reg.test(stu_id)) {
+        $("button.btn-default").css('backgroundColor', '#e6e6e6');
     }
-		$.ajax({
-		type:'POST', //请求方式，默认POST
-		url :"http://keepfriend.cn/kcb/course_schedule.php",
-		data:{stu_id:stu_id,stu_name:stu_name,status:status},
-        dataType: "json", 	
-        success:function(data){
-        	console.log(data.header);
-        	if(data.header.status=="0"){
-        		layer.msg(data.header.extra_info);
-        	}else if(data.header.status=="1"){
-        		window.location.href="course.html";
-        	}
-        	
-        },error:function(){
-        		layer.msg("导入出现错误！可能是网络问题");
-        },
-		});
-	});
-	
-	
+    $('#stu_id').on('input', function() {
+        stu_id = $("#stu_id").val();
+        stu_name = $("#stu_name").val();
+        if (!stu_id || !stu_name || !reg.test(stu_id)) {
+            $("button.btn-default").css('backgroundColor', '#e6e6e6');
+            return false;
+        } else {
+            $("button.btn-default").css('backgroundColor', '#d26465');
+        }
+    });
+    $('#stu_name').on('input', function() {
+        stu_id = $("#stu_id").val();
+        stu_name = $("#stu_name").val();
+        if (!stu_id || !stu_name || !reg.test(stu_id)) {
+            $("button.btn-default").css('backgroundColor', '#e6e6e6');
+            return false;
+        } else {
+            $("button.btn-default").css('backgroundColor', '#d26465');
+        }
+    })
+
+    $("button.btn-default").click(function() {
+        stu_id = $("#stu_id").val();
+        stu_name = $("#stu_name").val();
+        if (stu_id == '') {
+            layer.msg("学号不能为空");
+            return false;
+        }
+        if (!reg.test(stu_id)) {
+            layer.msg("请输入正确的学号格式");
+            return false;
+        }
+        if (stu_name == '') {
+            layer.msg("姓名不能为空");
+            return false;
+        }
+        layer.load(2);
+        setTimeout(function() {
+            layer.closeAll('loading');
+        }, 300);
+        if ($("#remember_me").is(':checked')) {
+            $.cookie("remember", "true", { expires: 180 });
+            $.cookie("stu_id", stu_id, { expires: 180 });
+            $.cookie("stu_name", stu_name, { expires: 180 });
+        } else {
+            $.cookie("remember", "false", { expire: 1 });
+            $.cookie("stu_id", stu_id, { expires: 1 });
+            $.cookie("stu_name", stu_name, { expires: 1 });
+        }
+
+        $.ajax({
+            type: 'POST',
+            url: "http://www.szer.me/kcb/course_schedule.php",
+            data: { 'stu_id': stu_id, stu_name: stu_name, status: status, 'token': token },
+            dataType: "json",
+            success: function(data) {
+                if (data.header.status == "0") {
+                    layer.msg(data.header.extra_info);
+                } else if (data.header.status == "1") {
+                    window.location.href = "course.html";
+                }
+            },
+            error: function(data) {
+                layer.msg("导入出现错误！请检查数据格式");
+            },
+        });
+    });
+
 });
-
-
